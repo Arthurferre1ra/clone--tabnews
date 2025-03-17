fe6287c4cb34aab638570a1fa2bf0dbfd8081bac
Arthur Ferreira <150069589+Arthurferre1ra@users.noreply.github.com>
2025-03-17 11:39:41 +0000

    adicionando a primeira página

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return (
+        <h1>Quando você sentir algum medo ou seu coração acelerar por estar passando por uma situação que não é nocíva, mas o seu cérebro projetou para que fosse, vá a partir do momento 
+            em que sentir esses sintomas. São nesses momentos em que conseguimos evoluir absurdamente, por mais que seja desconfotável</h1>
+    )
+}
+
+export default Home;
